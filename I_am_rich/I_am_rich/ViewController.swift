07762a60c6fd1d6e29b1b92b0a3b1a25289f5e93
Nando <Camarillo.itsl@gmail.com>
2019-02-26 20:57:44 -0600

    Proyect I am rich
    Proyect I am rich, I am learning about Swift indeed a Course is being learning by me

---
+++ I_am_rich/I_am_rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I_am_rich
+//
+//  Created by Nando on 2/26/19.
+//  Copyright © 2019 n0813. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
