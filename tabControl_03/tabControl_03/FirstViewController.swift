c9be67b5547e2a661351bef2918ab433dd397331
Nando <Camarillo.itsl@gmail.com>
2019-03-18 13:26:50 -0600

    tab Control
    tab control , ejemplos

---
+++ tabControl_03/tabControl_03/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  tabControl_03
+//
+//  Created by Nando on 3/18/19.
+//  Copyright © 2019 n0813. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
